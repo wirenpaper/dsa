78391cf056b1bd610ecd592e2ae71cbf1a0c8cef
wnp <chumchumchira@gmail.com>
2024-06-14 07:46:57 +0500

    dll: list: add_last implemented

--- 3_doubly_linked_list/libs/list.py
+++ 3_doubly_linked_list/libs/list.py
@@ -11,3 +11,10 @@ class List:
             self.tail = self.head
         else:
             self.head = self.head.add_first(data)
+
+    def add_last(self, data):
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail = self.head.add_last(self.tail, data)
--- 3_doubly_linked_list/tests/test_list.py
+++ 3_doubly_linked_list/tests/test_list.py
@@ -32,3 +32,44 @@ def test_add_first():
 
     assert mlist.head.next.prev.data == 5
     assert mlist.tail.prev.next.data == 3
+
+def test_add_last():
+    mlist = List()
+    assert mlist.head == None
+    assert mlist.tail == None
+
+    mlist.add_last(3)
+    assert mlist.head.data == 3
+    assert mlist.head.next == None
+    assert mlist.tail.data == 3
+    assert mlist.tail.prev == None
+
+    mlist.add_last(4)
+    assert mlist.head.data == 3
+    assert mlist.head.next.data == 4
+    assert mlist.head.next.next == None
+    assert mlist.tail.data == 4
+    assert mlist.tail.prev.data == 3
+    assert mlist.tail.prev.prev == None
+
+    mlist.add_last(5)
+    assert mlist.head.data == 3
+    assert mlist.head.next.data == 4
+    assert mlist.head.next.next.data == 5
+    assert mlist.head.next.next.next == None
+    assert mlist.tail.data == 5
+    assert mlist.tail.prev.data == 4
+    assert mlist.tail.prev.prev.data == 3
+    assert mlist.tail.prev.prev.prev == None
+
+    mlist.add_last(6)
+    assert mlist.head.data == 3
+    assert mlist.head.next.data == 4
+    assert mlist.head.next.next.data == 5
+    assert mlist.head.next.next.next.data == 6
+    assert mlist.head.next.next.next.next == None
+    assert mlist.tail.data == 6
+    assert mlist.tail.prev.data == 5
+    assert mlist.tail.prev.prev.data == 4
+    assert mlist.tail.prev.prev.prev.data == 3
+    assert mlist.tail.prev.prev.prev.prev == None
